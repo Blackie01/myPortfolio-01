4feb432959a7116b60a07a72ff519e5eccfeb0e7
Blackie01 <damola24795@gmail.com>
2023-01-13 21:30:49 +0100

    new update

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const myRoles = ["web developer", "technical writer", "content writer."];
+const myRoles = ["web developer", "technical writer"];
 let index = -1;
 setInterval(() => {
     index++;
     index %= myRoles.length;
     document.querySelector("#d").innerHTML = myRoles[index];
 }, 2000);
+
 
 
 
