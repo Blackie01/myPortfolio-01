f25b8157e456af8560037b5e79b3805fccf11b61
Blackie01 <onioludamola@gmail.com>
2024-02-28 23:48:49 +0100

    hero section fix

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const myRoles = ["web developer", "technical writer"];
-let index = -1;
-setInterval(() => {
-    index++;
-    index %= myRoles.length;
-    document.querySelector("#d").innerHTML = myRoles[index];
-}, 2000);
+// const myRoles = ["web developer", "technical writer"];
+// let index = -1;
+// setInterval(() => {
+//     index++;
+//     index %= myRoles.length;
+//     document.querySelector("#d").innerHTML = myRoles[index];
+// }, 2000);
 
 
 
